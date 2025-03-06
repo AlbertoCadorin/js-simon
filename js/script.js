eaa47e34cbd91bd806d84d96f0cd2e3c15f833b9
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-06 15:52:26 +0100

    feat:  add countdown

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-// lista numeri random
+// richiamo number list
 const numberListItems = document.getElementById('numbers-list');
-
+// salvo i numeri generati in una variabile 
 let number1 = randomNumbers();
 let number2 = randomNumbers();
 let number3 = randomNumbers();
 let number4 = randomNumbers();
 let number5 = randomNumbers();
-
+// lista numeri random
 numberListItems.innerHTML = `
 <li>${number1}</li>
 <li>${number2}</li>
@@ -14,6 +14,12 @@ numberListItems.innerHTML = `
 <li>${number4}</li>
 <li>${number5}</li>`
 
+//  richiamo il countdown
+const countdownElement = document.getElementById('countdown')
+const answersFormElement = document.getElementById('answers-form')
+// timer
+let count = 10
+let timer = setInterval(startTimer, 1000)
 
 
 
@@ -40,12 +46,20 @@ numberListItems.innerHTML = `
 
 
 
-
-
-
-
-// FUNZIONI 
+// FUNZIONI //
+// generare numeri 
 function randomNumbers(){
     return Math.floor(Math.random() * 50 ) + 1;
 }
 
+
+// taimer per far sparire i numeri 
+function startTimer(){
+    countdownElement.innerHTML = count
+    count --
+    if(count === 0){
+        countdownElement.className = 'd-none'
+        numberListItems.className = 'd-none'
+        answersFormElement.className = 'd-block'
+    }
+}
